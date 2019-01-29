8958528d98a6c2b95d5c50ca9d68f9ddb11080f8
PhillHenry <phillhenry@gmail.com>
2019-01-29 14:10:43 +0000

    Added JUnit annotation.

--- general/src/test/scala/uk/co/odinconsultants/features/random/KolmogorovSpec.scala
+++ general/src/test/scala/uk/co/odinconsultants/features/random/KolmogorovSpec.scala
@@ -1,7 +1,10 @@
 package uk.co.odinconsultants.features.random
 
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 import org.scalatest.{Matchers, WordSpec}
 
+@RunWith(classOf[JUnitRunner])
 class KolmogorovSpec extends WordSpec with Matchers {
 
   import Kolmogorov._
