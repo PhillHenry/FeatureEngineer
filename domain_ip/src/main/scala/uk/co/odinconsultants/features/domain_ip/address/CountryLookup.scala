0f0bca93e3cb228eca34706c83c6c7b5d4e5be5d
PhillHenry <phillhenry@gmail.com>
2019-03-08 10:34:17 +0000

    Added caching.

--- domain_ip/src/main/scala/uk/co/odinconsultants/features/domain_ip/address/CountryLookup.scala
+++ domain_ip/src/main/scala/uk/co/odinconsultants/features/domain_ip/address/CountryLookup.scala
@@ -2,13 +2,13 @@ package uk.co.odinconsultants.features.domain_ip.address
 
 import java.net.InetAddress
 
-import com.maxmind.db.Reader
+import com.maxmind.db.{CHMCache, Reader}
 
 object CountryLookup {
 
   val inputStream = this.getClass.getClassLoader.getResourceAsStream("GeoLite2-Country.mmdb")
 
-  val reader = new Reader(inputStream)
+  val reader = new Reader(inputStream, new CHMCache())
 
   /**
     * Returns the ISO code of the country to which this maps.
