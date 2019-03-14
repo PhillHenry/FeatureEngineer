03cef054b7d91f84308928702418d2a3c2c9a82b
PhillHenry <phillhenry@gmail.com>
2019-03-14 10:44:50 +0000

    Added a population distribution encoding.

--- general/src/main/scala/uk/co/odinconsultants/features/linalg/Encodings.scala
+++ general/src/main/scala/uk/co/odinconsultants/features/linalg/Encodings.scala
@@ -8,22 +8,26 @@ object Encodings {
 
   type EncodingFn[T] = (Vec[T], Vec[T]) => Vec[T]
 
-  def to1HotEncoding[T, U : Numeric : ClassTag](toEncode: Seq[T], space: Space[T, U]): Vec[U] = {
-    val appending: EncodingFn[U] = (acc, xs) => acc ++ xs
-    encode(toEncode, space, appending)
+  def toPopulationDistn[T, U : Numeric : ClassTag](xs: Seq[T], space: Space[T, U]): Vec[U] = {
+    val adding: EncodingFn[U] = (acc, xs) => acc.zip(xs).map { case (x, y) => space.op.plus(x, y) }
+    val seed                      = Array.fill(space.domain.size)(space.zero)
+    encode(xs, space, adding, seed)
   }
 
-  def encode[U: Numeric : ClassTag, T](x: Seq[T], space: Space[T, U], encoding: EncodingFn[U]): Vec[U] = {
-    val charVecs  = to1HotVectors(x, space)
-    val seed      = Array.fill(0)(space.zero)
-    charVecs.foldLeft(seed)(encoding)
+  def to1HotEncoding[T, U : Numeric : ClassTag](xs: Seq[T], space: Space[T, U]): Vec[U] = {
+    val appending: EncodingFn[U]  = (acc, xs) => acc ++ xs
+    val seed                      = Array.fill(0)(space.zero)
+    encode(xs, space, appending, seed)
   }
 
-  def to1HotVectors[T, U : Numeric : ClassTag](x: Seq[T], space: Space[T, U]): Seq[Vec[U]] = {
+  def encode[U: Numeric : ClassTag, T](xs: Seq[T], space: Space[T, U], encoding: EncodingFn[U], seed: Vec[U]): Vec[U] =
+    to1HotVectors(xs, space).foldLeft(seed)(encoding)
+
+  def to1HotVectors[T, U : Numeric : ClassTag](xs: Seq[T], space: Space[T, U]): Seq[Vec[U]] = {
 
     def emptyVector: Vec[U] = Array.fill(space.domain.size)(space.zero)
 
-    x.map { x =>
+    xs.map { x =>
       val arr = emptyVector
       arr(space.toIndex(x)) = space.op.fromInt(1)
       arr
---
+++ general/src/test/scala/uk/co/odinconsultants/features/linalg/EncodingsSpec.scala
@@ -0,0 +1,39 @@
+package uk.co.odinconsultants.features.linalg
+
+import org.scalatest.{Matchers, WordSpec}
+
+import scala.collection.immutable.IndexedSeq
+import uk.co.odinconsultants.features.linalg.Space
+
+class EncodingsSpec extends WordSpec with Matchers {
+
+  import Encodings._
+
+  val domain: IndexedSeq[Char]  = ('0' to '9')
+  val space                     = Space[Char, Double](domain)
+  val testString                = "123"
+
+  "One hot encoding vectors" should {
+    "be present for each element" in {
+      to1HotVectors(testString, space) should have size testString.length
+    }
+    "be the same size as the domain" in {
+      to1HotVectors(testString, space).foreach { vec =>
+        vec should have length domain.length
+      }
+    }
+  }
+
+  "one hot encoding" should {
+    "have size (n * |domain|)" in {
+      to1HotEncoding(testString, space) should have size (testString.length * domain.length)
+    }
+  }
+
+  "Population distribution" should {
+    "have the same length as the features" in {
+      toPopulationDistn(testString, space) should have length domain.length
+    }
+  }
+
+}
