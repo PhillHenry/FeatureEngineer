0f201c5c17b5dca2a7b9098f5bb35cc1b939f4ce
PhillHenry <phillhenry@gmail.com>
2019-04-03 14:47:41 +0100

    All DNSs, some tests.

--- domain_ip/src/test/scala/uk/co/odinconsultants/features/domain_ip/address/DomainNameRegistrySpec.scala
+++ domain_ip/src/test/scala/uk/co/odinconsultants/features/domain_ip/address/DomainNameRegistrySpec.scala
@@ -19,9 +19,22 @@ class DomainNameRegistrySpec extends WordSpec with Matchers {
 
   "An attempt to parse" should {
     val badDomain = InternetDomainName.from("x")
-    val goodDNS   = InetAddress.getByName("whois.networksolutions.com")
+    val goodDNS   = "whois.networksolutions.com"
     "yield none for bad domains" in {
       attemptParse(badDomain, goodDNS) shouldBe None
+    }
+  }
+
+  "The mappings between TLDs and DNS" should {
+    val com   = "com"
+    val coUk  = "co.uk"
+    val dns   = Set("x." + com, "y." + com, "z." + coUk)
+    val tlds  = Set(com, coUk)
+    "provide DNSs grouped by TLD" in {
+      val mappings = tldToDNS(dns, tlds)
+      mappings should have size 2
+      mappings(com) should have size 2
+      mappings(coUk) should have size 1
     }
   }
 
