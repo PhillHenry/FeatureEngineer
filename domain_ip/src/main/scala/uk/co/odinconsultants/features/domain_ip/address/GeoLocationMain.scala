ab02be3a5aff6bd8f2b8f44c647a2055096be79e
PhillHenry <phillhenry@gmail.com>
2019-04-16 15:37:35 +0100

    Consistent logging.

--- domain_ip/src/main/scala/uk/co/odinconsultants/features/domain_ip/address/GeoLocationMain.scala
+++ domain_ip/src/main/scala/uk/co/odinconsultants/features/domain_ip/address/GeoLocationMain.scala
@@ -6,12 +6,13 @@ object GeoLocationMain {
 
   def main(args: Array[String]): Unit = {
     val ipAddresses = Source.fromFile(args(0)).getLines()
+    val format = "%11.6f"
     ipAddresses.foreach { ip =>
       val country   = CountryLookup(ip)
       val location  = CityLookup(ip)
       println("%-20s%-5s%30s".format(ip,
         country.right.getOrElse(""),
-        location.right.map(x => "Lat = %3f   Long = %11.6f".format(x.lat, x.long)).right.getOrElse(""))
+        location.right.map(x => s"Lat = $format   Long = $format".format(x.lat, x.long)).right.getOrElse(""))
       )
     }
   }
