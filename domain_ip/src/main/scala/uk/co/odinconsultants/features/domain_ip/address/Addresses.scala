728e1eda22ff5503707c714e7f27d73ab511ea4c
PhillHenry <phillhenry@gmail.com>
2019-04-25 10:55:34 +0100

    Removing ports.

--- domain_ip/src/main/scala/uk/co/odinconsultants/features/domain_ip/address/Addresses.scala
+++ domain_ip/src/main/scala/uk/co/odinconsultants/features/domain_ip/address/Addresses.scala
@@ -2,6 +2,14 @@ package uk.co.odinconsultants.features.domain_ip.address
 
 object Addresses {
 
+  def removePorts(x: String): String = {
+    val reg = """(.+):\d+""".r
+    x match {
+      case reg(y) => y
+      case _  => x
+    }
+  }
+
   def isIPAddress(x: String): Boolean = {
     val reg = """.*(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3}).*""".r
     x match {
--- domain_ip/src/test/scala/uk/co/odinconsultants/features/domain_ip/address/AddressesSpec.scala
+++ domain_ip/src/test/scala/uk/co/odinconsultants/features/domain_ip/address/AddressesSpec.scala
@@ -9,6 +9,13 @@ class AddressesSpec extends WordSpec with Matchers {
 
   import Addresses._
 
+  "hostname with port" should {
+    val x = "bbc.co.uk"
+    "have its port removed" in {
+      removePorts(s"$x:8080") shouldBe x
+    }
+  }
+
   "IP address by itself" should {
     "be recognized as an IP address" in {
       isIPAddress("127.0.0.1") shouldBe true
@@ -18,6 +25,9 @@ class AddressesSpec extends WordSpec with Matchers {
   "IP address with port" should {
     "be recognized as an IP address" in {
       isIPAddress("127.0.0.1:38632") shouldBe true
+    }
+    "have its port removed" in {
+      removePorts("127.0.0.1:38632") shouldBe "127.0.0.1"
     }
   }
 
