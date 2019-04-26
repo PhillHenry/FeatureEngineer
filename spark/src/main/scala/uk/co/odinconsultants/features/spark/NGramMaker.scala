afc5a052deb17e0492c7c3e2d4a4b6c66ae1c421
PhillHenry <PhillHenry@gmail.com>
2019-04-26 13:40:00 +0100

    NGrams from Zeppelin.

---
+++ spark/src/main/scala/uk/co/odinconsultants/features/spark/NGramMaker.scala
@@ -0,0 +1,17 @@
+package uk.co.odinconsultants.features.spark
+
+import org.apache.spark.sql.DataFrame
+import uk.co.odinconsultants.features.data.NGrams
+import org.apache.spark.sql.functions.sum
+
+object NGramMaker {
+
+  def toHistogram(words: DataFrame, n: Int): DataFrame = {
+    import words.sparkSession.implicits._
+    val nGramFn                   = NGrams.NGram(n)
+    val ngrams  = words.rdd.flatMap(r => nGramFn(r)).cache()
+    val counts  = ngrams.map(_ -> 1).reduceByKey(_ + _).toDF("ngram", "count")
+    val total   = counts.agg(sum('count)).collect()(0).getLong(0).toDouble
+  }
+
+}
