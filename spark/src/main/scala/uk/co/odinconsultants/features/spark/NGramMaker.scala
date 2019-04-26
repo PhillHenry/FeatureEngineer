7fbeb8b80be3600f284c9069254a73797299c256
PhillHenry <phillhenry@gmail.com>
2019-04-26 14:22:33 +0100

    Oops! fixed.

--- spark/src/main/scala/uk/co/odinconsultants/features/spark/NGramMaker.scala
+++ spark/src/main/scala/uk/co/odinconsultants/features/spark/NGramMaker.scala
@@ -8,10 +8,16 @@ object NGramMaker {
 
   def toHistogram(words: DataFrame, n: Int): DataFrame = {
     import words.sparkSession.implicits._
-    val nGramFn                   = NGrams.NGram(n)
-    val ngrams  = words.rdd.flatMap(r => nGramFn(r)).cache()
-    val counts  = ngrams.map(_ -> 1).reduceByKey(_ + _).toDF("ngram", "count")
-    val total   = counts.agg(sum('count)).collect()(0).getLong(0).toDouble
+    val nGramFn = NGrams.NGram(n)
+    val ngrams = words.rdd.flatMap(r => nGramFn(r.getString(0))).cache()
+    val counts = ngrams.map(_ -> 1).reduceByKey(_ + _).toDF("ngram", "count")
+    counts
+  }
+
+  def toProbabilities(counts: DataFrame): Double = {
+    import counts.sparkSession.implicits._
+    val total     = counts.agg(sum('count)).collect()(0).getLong(0).toDouble
+    total
   }
 
 }
