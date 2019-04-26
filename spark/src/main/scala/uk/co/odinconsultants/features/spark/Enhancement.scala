bc0ccda787943850797665bd928efc9afee47ee6
PhillHenry <phillhenry@gmail.com>
2019-04-26 15:38:53 +0100

    Return the enhanced DF.

--- spark/src/main/scala/uk/co/odinconsultants/features/spark/Enhancement.scala
+++ spark/src/main/scala/uk/co/odinconsultants/features/spark/Enhancement.scala
@@ -9,27 +9,30 @@ import org.apache.spark.sql.DataFrame
 
 object Enhancement {
 
-  def enhanceAndRegister(df: DataFrame, tlds: Set[String], table: String): Unit = Tld2DnsParser.readMappings.right.foreach { mappings =>
-    val t2d           = sortByLongestTLD(mappings.toSeq)
-    val orderedTLDs   = longestToShortest(tlds)
+  def enhanceAndRegister(df: DataFrame, tlds: Set[String], table: String): DataFrame = {
+    Tld2DnsParser.readMappings.right.map { mappings =>
+      val t2d = sortByLongestTLD(mappings.toSeq)
+      val orderedTLDs = longestToShortest(tlds)
 
-    def toWhois(x: String): String = {
-      val domain = clean(orderedTLDs, x)
-      suitableDNSFor(domain, t2d).flatMap(dns => apacheWhoIs(dns, domain).map(_._1)).getOrElse("-").toString
-    }
+      def toWhois(x: String): String = {
+        val domain = clean(orderedTLDs, x)
+        suitableDNSFor(domain, t2d).flatMap(dns => apacheWhoIs(dns, domain).map(_._1)).getOrElse("-").toString
+      }
 
-    val toWhoisUDF    = udf(toWhois _)
-    val latUDF        = udf(latitude _)
-    val longUDF       = udf(longitude _)
-    val countryUDF    = udf(lookupCountry _)
+      val toWhoisUDF = udf(toWhois _)
+      val latUDF = udf(latitude _)
+      val longUDF = udf(longitude _)
+      val countryUDF = udf(lookupCountry _)
 
-    df
-      .withColumn("whois",   toWhoisUDF(col("destinationHostName")))
-      .withColumn("lat",     latUDF(col("destinationAddress")))
-      .withColumn("lon",     longUDF(col("destinationAddress")))
-      .withColumn("country", countryUDF(col("destinationAddress")))
-      .cache()
-      .createOrReplaceTempView("enhanced")
+      val enhanced = df
+        .withColumn("whois", toWhoisUDF(col("destinationHostName")))
+        .withColumn("lat", latUDF(col("destinationAddress")))
+        .withColumn("lon", longUDF(col("destinationAddress")))
+        .withColumn("country", countryUDF(col("destinationAddress")))
+        .cache()
+      enhanced.createOrReplaceTempView("enhanced")
+      enhanced
+    }.right.get
   }
 
 }
