8b3fd72b0618ec4257561e5919204979d3c94657
PhillHenry <phillhenry@gmail.com>
2019-05-02 14:03:10 +0100

    Added a timeout.

--- spark/src/main/scala/uk/co/odinconsultants/features/spark/Enhancement.scala
+++ spark/src/main/scala/uk/co/odinconsultants/features/spark/Enhancement.scala
@@ -1,5 +1,7 @@
 package uk.co.odinconsultants.features.spark
 
+import java.lang.Thread.currentThread
+
 import uk.co.odinconsultants.features.domain_ip.address.{HttpChecker, Tld2DnsParser}
 import org.apache.spark.sql.functions.{col, udf}
 import uk.co.odinconsultants.features.domain_ip.address.DomainNameRegistry._
@@ -7,8 +9,12 @@ import uk.co.odinconsultants.features.domain_ip.address.CityLookup._
 import uk.co.odinconsultants.features.domain_ip.address.CountryLookup._
 import org.apache.spark.sql.DataFrame
 import java.sql.Timestamp
+import java.util.concurrent.ConcurrentHashMap
+import java.util.Collections
 
 import uk.co.odinconsultants.features.domain_ip.address.HttpChecker.httpCodeOf
+
+import scala.concurrent.{ExecutionContext, Future}
 
 object Enhancement {
 
@@ -17,14 +23,21 @@ object Enhancement {
 
   def enhanceAndRegister(df: DataFrame, tlds: Set[String], table: String): DataFrame = {
     Tld2DnsParser.readMappings.right.map { mappings =>
+      implicit val ec = ExecutionContext.global
+
+      val timeoutMs   = 4000
       val t2d         = sortByLongestTLD(mappings.toSeq)
       val orderedTLDs = longestToShortest(tlds)
       val toWhoIs     = whoIsFn(orderedTLDs, t2d)
-      val toWhoisUDF  = udf(toWhoIs)
+      val toWhoisUDF  = udf({ x: String =>
+        stopMe(toWhoIs(x), timeoutMs)
+      })
       val latUDF      = udf(latitude _)
       val longUDF     = udf(longitude _)
       val countryUDF  = udf(lookupCountry _)
-      val httpCodeUDF = udf[Int, String, Int](httpCodeOf(_, _))
+      val httpCodeUDF = udf[Int, String, Int]({ case (h, p) =>
+        stopMe(httpCodeOf(h, p), timeoutMs)
+      } )
 
       val enhanced = df
         .withColumn("whois",    toWhoisUDF(col("destinationHostName")))
@@ -38,9 +51,36 @@ object Enhancement {
     }.right.get
   }
 
+  def stopMe[T](f: => T, timeout: Long)(implicit ec: ExecutionContext): T = {
+    import scala.collection.JavaConversions._
+
+    val threads = Collections.newSetFromMap[Thread](new ConcurrentHashMap[Thread, Boolean]().asInstanceOf[java.util.Map[Thread, java.lang.Boolean]])
+    threads.add(currentThread())
+
+    val mutex = new Object
+
+    Future {
+      Thread.sleep(timeout)
+      mutex.synchronized {
+        threads.foreach { t =>
+          t.interrupt()
+          threads.remove(t)
+        }
+      }
+    }
+
+    val t = f
+
+    mutex.synchronized {
+      if (!threads.remove(currentThread())) Thread.interrupted()
+    }
+
+    t
+  }
+
   def whoIsFn(orderedTLDs: Seq[String], t2d: Seq[TLD2Domain]): String => Timestamp = { x =>
     import scala.collection.convert.decorateAsScala._
-    val x2Whois = new java.util.concurrent.ConcurrentHashMap[String, Option[Timestamp]]().asScala
+    val x2Whois = new ConcurrentHashMap[String, Option[Timestamp]]().asScala
 
     def whoisAndCache(x: String): Option[Timestamp] = {
       val domain    = clean(orderedTLDs, x)
--- spark/src/test/scala/uk/co/odinconsultants/features/spark/EnhancementSpec.scala
+++ spark/src/test/scala/uk/co/odinconsultants/features/spark/EnhancementSpec.scala
@@ -2,15 +2,39 @@ package uk.co.odinconsultants.features.spark
 
 import org.scalatest.{Matchers, WordSpec}
 
+import scala.concurrent.ExecutionContext
+
 class EnhancementSpec extends WordSpec with Matchers {
+
+  implicit val ec = ExecutionContext.global
 
   import Enhancement._
 
-  val now = new java.sql.Date(new java.util.Date().getTime)
+  val CANCELLED = new Object
+  val SUCCESS = new Object
 
-  "The 'nNoOp' data" should {
-    "be further in the future than now" in {
-//      NoDate.after(now) shouldBe true
+  def longRunningFn: Object = {
+    try {
+      Thread.sleep(Long.MaxValue)
+      SUCCESS
+    } catch {
+      case e: InterruptedException => CANCELLED
+    }
+  }
+
+  def fastFunction: Object = SUCCESS
+
+  "Long running thread" should {
+    "be timed out" in {
+      stopMe(longRunningFn, 200) shouldBe CANCELLED
+      Thread.interrupted() shouldBe false
+    }
+  }
+
+  "Fast thread" should {
+    "return a value" in {
+      stopMe(fastFunction, 200) shouldBe SUCCESS
+      Thread.interrupted() shouldBe false
     }
   }
 
