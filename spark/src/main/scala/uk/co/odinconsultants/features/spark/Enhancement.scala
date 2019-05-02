afc100855c6ca7e99879d704e67c1e5601429eec
PhillHenry <phillhenry@gmail.com>
2019-05-02 09:46:33 +0100

    Refactoring.

--- spark/src/main/scala/uk/co/odinconsultants/features/spark/Enhancement.scala
+++ spark/src/main/scala/uk/co/odinconsultants/features/spark/Enhancement.scala
@@ -15,37 +15,37 @@ object Enhancement {
 
   def enhanceAndRegister(df: DataFrame, tlds: Set[String], table: String): DataFrame = {
     Tld2DnsParser.readMappings.right.map { mappings =>
-      val t2d = sortByLongestTLD(mappings.toSeq)
+      val t2d         = sortByLongestTLD(mappings.toSeq)
       val orderedTLDs = longestToShortest(tlds)
-
-      import scala.collection.convert.decorateAsScala._
-      val x2Whois = new java.util.concurrent.ConcurrentHashMap[String, Option[Timestamp]]().asScala
-
-      def whoisAndCache(x: String): Option[Timestamp] = {
-        val domain    = clean(orderedTLDs, x)
-        val maybeDate = suitableDNSFor(domain, t2d).flatMap(dns => toMaybeDate(dns, domain))
-        x2Whois(x)    = maybeDate
-        maybeDate
-      }
-
-      def toWhois(x: String): Timestamp = {
-        x2Whois.getOrElse(x, whoisAndCache(x)).getOrElse(null)
-      }
-
-      val toWhoisUDF  = udf(toWhois _)
+      val toWhoIs     = whoIsFn(orderedTLDs, t2d)
+      val toWhoisUDF  = udf(toWhoIs)
       val latUDF      = udf(latitude _)
       val longUDF     = udf(longitude _)
       val countryUDF  = udf(lookupCountry _)
 
       val enhanced = df
-        .withColumn("whois", toWhoisUDF(col("destinationHostName")))
-        .withColumn("lat", latUDF(col("destinationAddress")))
-        .withColumn("lon",  longUDF(col("destinationAddress")))
-        .withColumn("country", countryUDF(col("destinationAddress")))
+        .withColumn("whois",    toWhoisUDF(col("destinationHostName")))
+        .withColumn("lat",      latUDF(col("destinationAddress")))
+        .withColumn("lon",      longUDF(col("destinationAddress")))
+        .withColumn("country",  countryUDF(col("destinationAddress")))
         .cache()
       enhanced.createOrReplaceTempView(table)
       enhanced
     }.right.get
+  }
+
+  def whoIsFn(orderedTLDs: Seq[String], t2d: Seq[TLD2Domain]): String => Timestamp = { x =>
+    import scala.collection.convert.decorateAsScala._
+    val x2Whois = new java.util.concurrent.ConcurrentHashMap[String, Option[Timestamp]]().asScala
+
+    def whoisAndCache(x: String): Option[Timestamp] = {
+      val domain    = clean(orderedTLDs, x)
+      val maybeDate = suitableDNSFor(domain, t2d).flatMap(dns => toMaybeDate(dns, domain))
+      x2Whois(x)    = maybeDate
+      maybeDate
+    }
+
+    x2Whois.getOrElse(x, whoisAndCache(x)).getOrElse(null)
   }
 
 }
