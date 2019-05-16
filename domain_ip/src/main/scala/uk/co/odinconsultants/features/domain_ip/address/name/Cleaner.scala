a0331707cb01dd2bc6442f531a54c12217b1fc48
PhillHenry <phillhenry@gmail.com>
2019-05-16 15:41:50 +0100

    Mappings to ascii work.

--- domain_ip/src/main/scala/uk/co/odinconsultants/features/domain_ip/address/name/Cleaner.scala
+++ domain_ip/src/main/scala/uk/co/odinconsultants/features/domain_ip/address/name/Cleaner.scala
@@ -5,26 +5,33 @@ import uk.co.odinconsultants.features.domain_ip.util.Reading
 
 object Cleaner {
 
-  val ascii:          Seq[Char]         = ('0' to '9') ++ ('a' to 'z') ++ ('A' to 'Z') :+ '.' :+ '-'
-  val asciiUnicde:    Set[String]       = ascii.map(_.toString).toSet
-  val unicode2Ascii:  Map[String, Char] = ascii.map(x => x.toString -> x).toMap
+  val domainAscii:    Seq[Char]         = ('0' to '9') ++ ('a' to 'z') ++ ('A' to 'Z') :+ '.' :+ '-'
+  val asciiUnicde:    Set[String]       = domainAscii.map(_.toString).toSet
+  val unicode2Ascii:  Map[String, Char] = domainAscii.map(x => x.toString -> x).toMap
 
-  val x2Ascii: Map[String, Char] = Reading.fromClasspath("char_codes.txt").flatMap { line =>
-    val xs    = line.split(",")
-    val aOpt  = xs.find(asciiUnicde.contains(_))
+  def int2Unicode(i: Int): String =
+    Character.toString(i.toChar)
+
+  def csv2Unicode(line: String): Seq[String] = if (line.startsWith("#")) Seq() else
+    line.split(",").map(x => Integer.parseInt(x, 16)).map(int2Unicode)
+
+  val csv2Mappings: String => Seq[(String, Char)] = { line =>
+    val xs    = csv2Unicode(line)
+    val aOpt  = xs.find(asciiUnicde.contains)
     mappings(aOpt, xs)
-  }.toMap
+  }
 
   def mappings(aOpt: Option[String], xs: Seq[String]): Seq[(String, Char)] =
     aOpt.map { a =>
       xs.filterNot(_ == a).map(_ -> unicode2Ascii(a))
     }.toList.flatten
 
-  def toSimilarAscii(xs: String): String = {
+  val x2Ascii: Map[String, Char] = Reading.fromClasspath("char_codes.txt").flatMap (csv2Mappings).toMap
+
+  def toSimilarAscii(xs: String): String =
     xs.map { x =>
       x2Ascii.getOrElse(x.toString, x)
     }
-  }
 
   def clean(x: String): String = {
     val ascii = if (x.startsWith("xn--")) java.net.IDN.toUnicode(x) else x
--- domain_ip/src/test/scala/uk/co/odinconsultants/features/domain_ip/address/name/CleanerSpec.scala
+++ domain_ip/src/test/scala/uk/co/odinconsultants/features/domain_ip/address/name/CleanerSpec.scala
@@ -9,12 +9,24 @@ class CleanerSpec extends WordSpec with Matchers {
 
   import Cleaner._
 
+  "CVS" should {
+    "map to single unicode characters" in {
+      val xs = csv2Unicode("5f,7fa,fe4d,fe4e,fe4f,ff3f")
+      xs should have size 6
+      xs foreach { x =>
+        withClue(s"$x in $xs") {
+          x.length shouldBe 1
+        }
+      }
+    }
+  }
+
   "Non-ascii characters" should {
-    "map to ascii" ignore new VodafoneFixture {
-      similar foreach { xs =>
-        toSimilarAscii(xs) foreach { x =>
-          withClue(xs) {
-            asciiUnicde should contain (x.toString)
+    "map to ascii" in new VodafoneFixture {
+      similar foreach { w =>
+        toSimilarAscii(w) foreach { c =>
+          withClue(s"%04X ($c) in $w instead maps to: ${x2Ascii.getOrElse(c.toString, "unknown")}\n${x2Ascii.keys.map(_.charAt(0)).toList.sorted.map(_.toHexString).mkString("\n")}\n".format(c & 0xFFFFF)) {
+            asciiUnicde should contain (c.toString)
           }
         }
       }
